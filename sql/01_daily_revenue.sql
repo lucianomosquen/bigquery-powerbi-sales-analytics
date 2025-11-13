b591c9279efaeb36f021710c0a2048bd7a5f27f9
Luciano Mosquén <luciano.mosquen@gmail.com>
2025-11-13 11:25:47 -0300

    Update 01_daily_revenue.sql

--- sql/01_daily_revenue.sql
+++ sql/01_daily_revenue.sql
@@ -1,11 +1,15 @@
+-- 01_daily_revenue.sql
 -- Daily Revenue: Google Analytics Sample
+
 SELECT
-  DATE(date) AS fecha,
+  PARSE_DATE('%Y%m%d', date) AS fecha,
   SUM(totals.totalTransactionRevenue) / 1000000 AS revenue_usd,
   SUM(totals.transactions) AS total_transactions,
   COUNT(*) AS sesiones
 FROM
   `bigquery-public-data.google_analytics_sample.ga_sessions_*`
+WHERE
+  totals.totalTransactionRevenue IS NOT NULL
 GROUP BY
   fecha
 ORDER BY
